281fabb714a2a4ff14598bca3c62f207ae9511b0
Vinicius Faria de Souza <vinicius.faria@tqi.com.br>
2019-03-15 09:09:21 -0300

    configurando swagger e incluido primeiro resource

---
+++ src/main/java/br/com/vfs/providermock/resource/ProductResource.java
@@ -0,0 +1,10 @@
+package br.com.vfs.providermock.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductResource {
+
+}
