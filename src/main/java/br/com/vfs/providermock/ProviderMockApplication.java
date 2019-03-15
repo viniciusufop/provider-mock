2989aa71b82848890573d42adca3f7e203da9f4c
Vinicius Faria de Souza <vinicius.faria@tqi.com.br>
2019-03-15 09:00:28 -0300

    Criando o projeto de mock de fornecedores

---
+++ src/main/java/br/com/vfs/providermock/ProviderMockApplication.java
@@ -0,0 +1,13 @@
+package br.com.vfs.providermock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vfs/providermock/ProviderMockApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vfs.providermock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProviderMockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
